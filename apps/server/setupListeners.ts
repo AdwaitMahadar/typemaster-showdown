7d03cf3e3bbf37b41e18e5478af4b6baad0415db
AdwaitMahadar <ad.learnspace12@gmail.com>
2024-05-26 12:49:45 -0700

    add setupListeners.ts with socket listeners and emitters

---
+++ apps/server/setupListeners.ts
@@ -0,0 +1,25 @@
+import { Server } from "socket.io";
+import { Game } from "./classes/game";
+
+const rooms = new Map<string, Game>();
+
+export function setupListeners(io: Server) {
+    io.on("connection", (socket) => {
+        console.log(`New connection: ${socket.id}`);
+
+        socket.on("join-game", (roomId: string, name: string) => {
+            if(!roomId) return socket.emit("error", "Invalid room ID");
+            if(!name) return socket.emit("error", "Please provide nickname");
+
+            socket.join(roomId);
+
+            if(rooms.has(roomId)) {
+                const game = rooms.get(roomId);
+                if(!game) return socket.emit("error", "Game not found");
+            }else {
+
+            }
+
+        })
+    })
+}
