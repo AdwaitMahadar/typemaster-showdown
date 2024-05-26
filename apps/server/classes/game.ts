09a6ee39a7c0cfa713227b1961ec79f2fda0aa34
AdwaitMahadar <ad.learnspace12@gmail.com>
2024-05-26 13:11:33 -0700

    add game class and configure it

--- apps/server/classes/game.ts
+++ apps/server/classes/game.ts
@@ -1,1 +1,21 @@
-export class Game {}
+import { Server, Socket } from "socket.io";
+
+export class Game {
+    gameStatus: "not-started" | "in-progress" | "finished";
+    gameId: string;
+    players: { id: string; score: number; name: string} [];
+    io: Server;
+    gameHost: string;
+    paragraph: string;
+
+    constructor(io: Server, id: string, host: string) {
+        this.io = io;
+        this.gameId = id;
+        this.gameHost = host;
+        this.gameStatus = "not-started";
+        this.players = [];
+        this.paragraph = "";
+    }
+
+    setupListeners(socket: Socket) {}
+}
